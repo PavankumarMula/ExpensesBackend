fc9e7883bb23f3add213c66e204129ec4a75576a
PavankumarMula <pavanpappy7@gmail.com>
2023-08-02 15:59:37 +0530

    Security Added

--- DataBase/database.js
+++ DataBase/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: process.env.DB_DIALECT,
+  dialect: "mysql",
+  port: DB_PORT,
   host: process.env.DB_HOST,
 });
 
