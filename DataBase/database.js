58148ac40a43ef16be84acdef620103b9c433dab
PavankumarMula <pavanpappy7@gmail.com>
2023-08-02 16:02:09 +0530

    changes made

--- DataBase/database.js
+++ DataBase/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: "mysql",
-  port: DB_PORT,
+  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
 });
 
